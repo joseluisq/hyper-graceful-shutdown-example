f97d6a4ff548bc3cda225e6ce53ee4a3dce9c31f
Jose Quintana <joseluisquintana20@gmail.com>
2023-05-27 12:09:51 +0200

    chore: improve message passing across threads

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,75 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::oneshot::{channel, Receiver};
+use tokio::sync::Mutex;
 
-async fn handle_request(_: Request<Body>, num: usize) -> Result<Response<Body>, hyper::Error> {
+pub type Result<T = (), E = anyhow::Error> = anyhow::Result<T, E>;
+
+async fn handle_request(_: Request<Body>, num: usize) -> Result<Response<Body>> {
     Ok(Response::new(Body::from(format!("Server #{}", num))))
 }
 
-async fn run_server(addr: SocketAddr, num: usize) {
-    let server = Server::bind(&addr).serve(make_service_fn(move |_conn| async move {
-        Ok::<_, hyper::Error>(service_fn(move |req| async move {
-            handle_request(req, num).await
+async fn run_server(addr: SocketAddr, num: usize, receiver: Arc<Mutex<Option<Receiver<()>>>>) {
+    let server = Server::bind(&addr)
+        .serve(make_service_fn(move |_conn| async move {
+            Ok::<_, hyper::Error>(service_fn(move |req| async move {
+                handle_request(req, num).await
+            }))
         }))
-    }));
+        // Graceful shutdown
+        .with_graceful_shutdown(async {
+            println!("Server #{} is waiting for signal...", num);
+            if let Some(receiver) = &mut *receiver.lock().await {
+                receiver.await.ok();
+                println!("Stopping server #{}...", num);
+            }
+        });
 
+    println!("Server #{} is listening on {}", num, addr);
     if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
+        eprintln!("Server #{} error: {}", num, e);
     }
+    println!("Server #{} is done!", num);
 }
 
-#[tokio::main]
-async fn main() {
-    let addr1: SocketAddr = "127.0.0.1:8080".parse().unwrap();
-    let addr2: SocketAddr = "127.0.0.1:8081".parse().unwrap();
+fn main() -> Result {
+    // Our message passing
+    // single-producer, single consumer channel
+    let (sender, receiver) = channel::<()>();
 
-    let task1 = tokio::spawn(run_server(addr1, 1));
-    let task2 = tokio::spawn(run_server(addr2, 2));
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?
+        .block_on(async {
+            // Simuate signal sending
+            let signal = tokio::spawn(async {
+                // Wait 5 secs and send a signal
+                println!("Waiting 5 secs for to send the signal...");
+                std::thread::sleep(std::time::Duration::new(5, 0));
+                println!("Termination signal sent!");
+                let _ = sender.send(());
+            });
 
-    tokio::try_join!(task1, task2).unwrap();
+            // Server 1
+            let receiver1 = Arc::new(Mutex::new(Some(receiver)));
+            let receiver2 = receiver1.clone();
+
+            let addr1: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+            let task1 = tokio::spawn(run_server(addr1, 1, receiver1));
+
+            // Server 2
+            let addr2: SocketAddr = "127.0.0.1:8081".parse().unwrap();
+            let task2 = tokio::spawn(run_server(addr2, 2, receiver2));
+
+            // Join all concurrent branches
+            if let Err(err) = tokio::try_join!(signal, task1, task2) {
+                eprintln!("server failed to start up: {:?}", err);
+            }
+
+            println!("All servers were shut down correctly!");
+        });
+
+    Ok(())
 }
